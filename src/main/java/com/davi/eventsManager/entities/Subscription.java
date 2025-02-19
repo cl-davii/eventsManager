feda826c9ec3f6e7c663340aa34f7b6aa3567bc4
Davi <davifsc03@gmail.com>
2025-02-19 19:48:58 -0300

    Entidade Subscription para armazenamento no banco de dados

---
+++ src/main/java/com/davi/eventsManager/entities/Subscription.java
@@ -0,0 +1,31 @@
+package com.davi.eventsManager.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "subscriptions")
+public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subscription_number")
+    private Integer subscriptionNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    @ManyToOne
+    @JoinColumn(name = "subscribed_user_id")
+    private User subscriber;
+
+    @ManyToOne
+    @JoinColumn(name = "indication_user_id")
+    private User indication;
+}
