091daef8747c55e00e545bbd3fb9460020299984
Davi <davifsc03@gmail.com>
2025-02-19 19:57:01 -0300

    Exceção para conflitos de cadastros de usuários

---
+++ src/main/java/com/davi/eventsManager/exceptions/SubscriptionConflict.java
@@ -0,0 +1,9 @@
+package com.davi.eventsManager.exceptions;
+
+public class SubscriptionConflict extends RuntimeException {
+
+    public SubscriptionConflict(String message) {
+        super(message);
+    }
+
+}
