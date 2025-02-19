fe4d0c52516db8ee0eb2d72ffbe508a5b197c89b
Davi <davifsc03@gmail.com>
2025-02-19 19:49:18 -0300

    Entidade User para armazenamento no banco de dados

---
+++ src/main/java/com/davi/eventsManager/entities/User.java
@@ -0,0 +1,26 @@
+package com.davi.eventsManager.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+}
