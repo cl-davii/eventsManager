b938eba9bce46f20f30d8d77665101a2e2218289
Davi <davifsc03@gmail.com>
2025-02-20 20:42:39 -0300

    all

---
+++ src/test/java/com/davi/eventsManager/EventsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davi.eventsManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
