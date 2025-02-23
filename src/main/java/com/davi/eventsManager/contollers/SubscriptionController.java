a24db95da64bab312e806bcf5c99229c1266a7ae
Davi <davifsc03@gmail.com>
2025-02-22 23:00:28 -0300

    Função para remover um inscrito do evento implementada com o método HTTP delete

--- src/main/java/com/davi/eventsManager/contollers/SubscriptionController.java
+++ src/main/java/com/davi/eventsManager/contollers/SubscriptionController.java
@@ -1,22 +1,35 @@
 package com.davi.eventsManager.contollers;
 
 import com.davi.eventsManager.dto.ErrorMessage;
+import com.davi.eventsManager.dto.SubscriptionRankingItem;
 import com.davi.eventsManager.dto.SubscriptionResponse;
+import com.davi.eventsManager.entities.Event;
+import com.davi.eventsManager.entities.Subscription;
 import com.davi.eventsManager.entities.User;
 import com.davi.eventsManager.exceptions.EventNotFound;
 import com.davi.eventsManager.exceptions.SubscriptionConflict;
 import com.davi.eventsManager.exceptions.UserIndicatorNotFound;
+import com.davi.eventsManager.repositories.EventRepository;
+import com.davi.eventsManager.repositories.SubscriptionRepository;
 import com.davi.eventsManager.services.SubscriptionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class SubscriptionController {
 
     @Autowired
     private SubscriptionService subscriptionService;
+
+//    @Autowired
+//    private SubscriptionRepository subscriptionRepository;
+
+    @Autowired
+    private EventRepository eventRepository;
 
     @PostMapping({"/subscription/{prettyName}", "/subscription/{prettyName}/{userId}"})
     public ResponseEntity<?> createSubscription(
@@ -40,6 +53,11 @@ public class SubscriptionController {
         }
     }
 
+    @GetMapping("/subscription/all")
+    public List<Subscription> findAllSubscriptions() {
+        return subscriptionService.getAllSubscriptions();
+    }
+
     @GetMapping("/subscription/{prettyName}/ranking")
     public ResponseEntity<?> getRankingByEvent(@PathVariable String prettyName) {
         try {
@@ -57,4 +75,11 @@ public class SubscriptionController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorMessage(e.getMessage()));
         }
     }
+
+    @DeleteMapping("/subscription/{prettyName}/{userId}")
+    public ResponseEntity<?> deleteSubscriptionFromEvent(@PathVariable String prettyName, @PathVariable Integer userId) {
+        subscriptionService.deleteUserFromEvent(prettyName, userId);
+        return ResponseEntity.ok().body("User " + userId + " successfully deleted");
+    }
+
 }
